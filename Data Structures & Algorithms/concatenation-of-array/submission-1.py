47b76385b036621a57aa3e3588aaa3cf15b27547
Hai Nguyen <84216850+hainguyenphc@users.noreply.github.com>
2026-05-23 20:23:52 +0700

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.py
@@ -0,0 +1,17 @@
+# class Solution:
+#     def getConcatenation(self, nums: list[int]) -> list[int]:
+#         ans = list()
+#         for i in range(0, 2):
+#             for num in nums:
+#                 ans.append(num)
+#         return ans
+
+class Solution:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
+        return nums * 2
+
+
+# nums = [1, 4, 2]
+# solution = Solution()
+# ans = solution.getConcatenation(nums)
+# print(ans)
